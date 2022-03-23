338f560e465154a38175bc6cc128e97fb2b8868a
Vaibhav <vaibhavmsondur@gmail.com>
2022-03-23 17:00:39 +0530

    Vtiger Second Push

---
+++ com.crm.VtigerApplication/src/test/java/commandLine/CreateContactsTest.java
@@ -0,0 +1,10 @@
+package commandLine;
+
+import org.testng.annotations.Test;
+
+public class CreateContactsTest {
+	@Test
+	public void createContacts() {
+		System.out.println("Contacts Created");
+	}
+}
