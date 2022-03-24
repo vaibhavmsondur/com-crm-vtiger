3bbf455827ed29c4391366cc00168d4fafefaed5
Vaibhav <vaibhavmsondur@gmail.com>
2022-03-24 17:20:09 +0530

    Branch Commit

--- com.crm.VtigerApplication/src/test/java/commandLine/CreateCampaignsTest.java
+++ com.crm.VtigerApplication/src/test/java/commandLine/CreateCampaignsTest.java
@@ -6,5 +6,11 @@ public class CreateCampaignsTest {
 	@Test
 	public void createCampaigns() {
 		System.out.println("Campaigns created");
+		
+		System.out.println("Campaign  modified");
+		
+		
+		
+		
 	}
 }
