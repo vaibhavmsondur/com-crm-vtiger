088263c663ded14919d48f057f9cd6895d8d9388
Vaibhav <vaibhavmsondur@gmail.com>
2022-03-29 13:14:54 +0530

    Dimi push

---
+++ com.crm.VtigerApplication/src/test/java/practice/Dimi.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Dimi {
+	@Test
+	public void dingi() {
+		System.out.println("DIMI LRIT");
+		System.out.println("DIMI CIT");
+	}
+}
